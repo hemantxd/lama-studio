20a5b9f2116a6bbb05f98cc51440c5e741c0ad4b
Hemant Bisht <bishthemant169@gmail.com>
2026-05-28 23:16:41 +0530

    feat: implement ImageKit client singleton and add uploadBufferToImagekit function; create openai.ts file

--- lib/imagekit.ts
+++ lib/imagekit.ts
@@ -1,6 +1,7 @@
-import ImageKit, {toFile} from "@imagekit/nodejs";
+import ImageKit, {toFile}  from "@imagekit/nodejs";
 
-let client: InstanceType<typeof ImageKit>|null = null;
+
+let _client: InstanceType<typeof ImageKit>|null = null;
 
 
 // this is the singleton pattern which is used to create a single instance of the ImageKit client
@@ -10,7 +11,31 @@ function getClient(){
         _client = new ImageKit({
             privateKey: process.env.IMAGEKIT_PRIVATE_KEY!,
         });
-        return _client;
+        
     }
+    return _client;
 }
 
+export async function uploadBufferToImagekit(
+  params: {
+    buffer: Buffer;
+    fileName: string;
+    folder: string;
+    mimetype: string;
+  }
+) {
+    const client = getClient()!;
+    const file = await toFile(params.buffer, params.fileName, {type: params.mimetype});
+
+    const result = await client.files.upload({
+        file: file,
+        fileName: params.fileName,
+        folder: params.folder,
+        useUniqueFileName: true,
+    });
+
+    return {
+        url: result.url!,
+        fileId: result.fileId!,
+    };
+}
